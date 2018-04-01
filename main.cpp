2ecd4c5b19d87ba58a715a48c4b6899a03fdea96
Matthew Kobilas <mattkobi789@gmail.com>
2018-04-01 16:44:39 -0400

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
 
 #include "parser.h"
@@ -9,57 +10,68 @@ using namespace std;
 int lineNumber;
 string fileMessage;
 int errorcount = 0;
+bool hasError = false;
 
 void messageStart(int linenum) {
-	cout << fileMessage << linenum+1 << ":";
+    cout << fileMessage << linenum + 1 << ":";
 }
 
 void error(int linenum, const string& message) {
-	++errorcount;
-	messageStart(linenum);
-	cout << "Syntax error " << message << endl;
+    hasError = true;
+    errorcount++;
+    messageStart(linenum);
+    cout << message << endl;
 }
 
-void semanticError(int linenum, const string& message) {
-	messageStart(linenum);
-	cout << message << endl;
+map<string, int> intSymbols;
+map<string, string> strSymbols;
+map<int, string> errors;
+
+void postOrder(ParseTree * t) {
+    if (t == 0)
+        return;
+    if (t->getLeft() != 0) {
+        postOrder(t->getLeft());
+    }
+    if (t->getRight() != 0) {
+        postOrder(t->getRight());
+    }
+    t->eval();
 }
 
-//map<string,Variable> SymbolTable;
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	ifstream file;
-	istream *in = &cin;
+    ifstream file;
+    istream *in = &cin;
 
-	for( int i=1 ; i < argc; i++ ) {
-		string arg(argv[i]);
-		if( in != &cin ) {
-			cout << "TOO MANY FILES" << endl;
-			return 0;
-		}
-		fileMessage = arg + ":";
-		file.open(arg);
-		if( file.is_open() == false ) {
-			cout << arg << " FILE NOT FOUND" << endl;
-			return 0;
-		}
+    for (int i = 1; i < argc; i++) {
+        string arg(argv[i]);
+        if (in != &cin) {
+            cout << "TOO MANY FILES" << endl;
+            return 0;
+        }
+        fileMessage = arg + ":";
+        file.open(arg);
+        if (file.is_open() == false) {
+            cout << arg << " FILE NOT FOUND" << endl;
+            return 0;
+        }
 
-		in = &file;
-	}
+        in = &file;
+    }
 
-	ParseTree *tree = Prog( in );
+    ParseTree *tree = Prog(in);
 
-	if( tree == 0 || errorcount > 0 ) {
-		// there was some kind of parse error
-		return 1;
-	}
+    if (tree == 0 || errorcount > 0) {
+        return 1;
+    }
     postOrder(tree);
-
-	//extern map<string,Variable>	symbolTable;
-
-	return 0;
+    map<int, string>::iterator it;
+    for(it = errors.begin(); it != errors.end(); it++)
+    {
+        error(it->first, it->second);
+    }
+    return 0;
 }
 
 
